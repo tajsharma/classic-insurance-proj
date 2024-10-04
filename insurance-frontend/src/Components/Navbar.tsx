3d571cde754c313a67fd091aca0fcd94a3cfc269
Taj <sharma.tajinder01@gmail.com>
2024-10-04 14:58:51 -0500

    change hover color of the buttons, add contact

--- insurance-frontend/src/Components/Navbar.tsx
+++ insurance-frontend/src/Components/Navbar.tsx
@@ -4,18 +4,22 @@ import logo from '../Assets/cropped-classic-logo-1-768x117.png';
 
 const Navbar: React.FC = () => {
     return (
-      <nav className="bg-blue-800 p-4">
+      <nav className="bg-yellow-50 p-6">
         <div className="container mx-auto flex justify-between">
           <Link to="/" className="text-white text-xl">
-              <img src={logo} alt='Classin Insurance Agency logo' className='w-64'></img>
+              <img src={logo} alt='Classic Insurance Agency logo' className='w-80'></img>
           </Link>
           <div>
-            <Link to="/services" className="text-gray-300 hover:text-white mx-2">Services</Link>
-            <Link to="/auto" className="text-gray-300 hover:text-white mx-2">Auto</Link>
-            <Link to="/home" className="text-gray-300 hover:text-white mx-2">Home</Link>
-            <Link to="/life" className="text-gray-300 hover:text-white mx-2">Life</Link>
-            <Link to="/business" className="text-gray-300 hover:text-white mx-2">Business</Link>
-            <Link to="/admin" className="text-gray-300 hover:text-white mx-2">Admin</Link>
+            <Link to="/services" className="text-black-300 hover:text-orange-400 mx-2">Services</Link>
+            <Link to="/auto" className="text-black-300 hover:text-orange-400 mx-2">Auto</Link>
+            <Link to="/home" className="text-black-300 hover:text-orange-400 mx-2">Home</Link>
+            <Link to="/life" className="text-black-300 hover:text-orange-400 mx-2">Life</Link>
+            <Link to="/business" className="text-black-300 hover:text-orange-400 mx-2">Business</Link>
+            <Link to="/admin" className="text-black-300 hover:text-orange-400 mx-2">Admin</Link>
+          </div>
+
+          <div>
+            <Link to="/contact" className="text-orange-400 hover:text-black-300 border rounded-lg border-orange-400 p-2">Contact Us!</Link>
           </div>
         </div>
       </nav>

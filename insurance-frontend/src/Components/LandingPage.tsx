960a5d54e3454edd05a71e643f3bf5f7bef4f61d
Taj <sharma.tajinder01@gmail.com>
2024-10-04 14:59:23 -0500

    add background to the page and first div

--- insurance-frontend/src/Components/LandingPage.tsx
+++ insurance-frontend/src/Components/LandingPage.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import bgimage from '../Assets/family-matters.jpg'
 
 const LandingPage: React.FC = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-4xl font-bold text-center">Welcome to Classic Insurance</h1>
-      <p className="mt-4 text-lg text-center">We offer the best insurance services for your auto, home, life, and business needs.</p>
-    </div>
+    <section style={{
+      backgroundImage: `url(${bgimage})`,
+      backgroundSize: 'cover',       // Ensures the image covers the entire div
+      backgroundPosition: 'center',  // Centers the image
+      height: '100vh',               // Full screen height
+    }} className="relative flex flex-col items-start justify-start text-white">
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      
+      <div className="relative z-10 p-10">
+        <h1 className="text-4xl font-bold">Welcome to Classic Insurance</h1>
+        <p className="mt-4 text-lg">We offer the best insurance services for your auto, home, life, and business needs.</p>
+      </div>
+      
+    </section>
+
   );
 };
 

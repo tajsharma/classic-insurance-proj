711ae1002fc2c1ae27aeafbb997110d30881cf1c
Taj <sharma.tajinder01@gmail.com>
2024-10-04 14:59:06 -0500

    bg image to tailwind exports

--- insurance-frontend/tailwind.config.js
+++ insurance-frontend/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'family-picture': "url('/src/Assets/family-matters.jpg')",
+      }
+    },
   },
   plugins: [],
 }

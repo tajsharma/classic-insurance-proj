4f07bcb05690d582a1a9c6c2d35e4d2992cbcecb
Taj <sharma.tajinder01@gmail.com>
2024-10-04 15:42:41 -0500

    change header and caption size. change width

--- insurance-frontend/src/Components/LandingPage.tsx
+++ insurance-frontend/src/Components/LandingPage.tsx
@@ -11,9 +11,11 @@ const LandingPage: React.FC = () => {
     }} className="relative flex flex-col items-start justify-start text-white">
       <div className="absolute inset-0 bg-black opacity-50"></div>
       
-      <div className="relative z-10 p-10">
-        <h1 className="text-4xl font-bold">Welcome to Classic Insurance</h1>
-        <p className="mt-4 text-lg">We offer the best insurance services for your auto, home, life, and business needs.</p>
+      <div className="relative z-10 py-40 px-40 w-2/3">
+        <h1 className="text-6xl font-bold">
+          Welcome to the <span className="block text-orange-400">Classic Insurance Agency!</span>
+        </h1>
+        <p className="mt-4 text-3xl">Insurance shoudln't be difficult, click get a quote today to protect your assets with us.</p>
       </div>
       
     </section>

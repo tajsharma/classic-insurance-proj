a622065766e567bee2f03f7c4b6a6a57a691d673
Taj <sharma.tajinder01@gmail.com>
2024-10-05 17:57:22 -0500

    change width of textbox below header

--- insurance-frontend/src/Components/LandingPage.tsx
+++ insurance-frontend/src/Components/LandingPage.tsx
@@ -13,9 +13,9 @@ const LandingPage: React.FC = () => {
       
       <div className="relative z-10 py-40 px-40 w-2/3">
         <h1 className="text-6xl font-bold">
-          Welcome to the <span className="block text-orange-400">Classic Insurance Agency!</span>
+          Welcome to the <span className="block text-orange-400 py-4">Classic Insurance Agency!</span>
         </h1>
-        <p className="mt-4 text-3xl">Insurance shoudln't be difficult, click get a quote today to protect your assets with us.</p>
+        <p className="mt-4 text-3xl py-2 w-9/12">Insurance shoudln't be difficult, click below get to a quote today to protect your assets with us.</p>
       </div>
       
     </section>

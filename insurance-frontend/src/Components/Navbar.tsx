305be199c28e3749a4a977850bfec8f2c8285cc0
Taj <sharma.tajinder01@gmail.com>
2024-10-05 20:11:18 -0500

    add useNavigate to navigate to services from each page

--- insurance-frontend/src/Components/Navbar.tsx
+++ insurance-frontend/src/Components/Navbar.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../Assets/cropped-classic-logo-1-768x117.png'; 
 
 const Navbar: React.FC = () => {
+    const navigate = useNavigate();
+
+    const goToServices = (e: React.MouseEvent) =>{
+      e.preventDefault();
+      navigate('/');
+
+      setTimeout(()=>{
+        const servicesSection = document.getElementById('services');
+        if (servicesSection) {
+          servicesSection.scrollIntoView({ behavior: 'smooth' });  // Scroll to services
+        }},100);
+    };
+
     return (
-      <nav className="bg-yellow-50 p-6 fixed top-0 z-10 flex w-full ">
+      <nav className="bg-yellow-50 p-6 fixed top-0 z-50 flex w-full ">
         <div className="container mx-auto flex justify-between">
           <Link to="/" className="text-white text-xl">
               <img src={logo} alt='Classic Insurance Agency logo' className='w-80'></img>
           </Link>
           <div>
-            <Link to="/services" className="text-black-300 hover:text-orange-400 mx-2">Services</Link>
+            <a href="#services" onClick={goToServices} className="text-black-300 hover:text-orange-400 mx-2">Services</a>
             <Link to="/auto" className="text-black-300 hover:text-orange-400 mx-2">Auto</Link>
             <Link to="/home" className="text-black-300 hover:text-orange-400 mx-2">Home</Link>
             <Link to="/life" className="text-black-300 hover:text-orange-400 mx-2">Life</Link>

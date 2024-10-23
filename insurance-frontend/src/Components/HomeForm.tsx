8998f156fbfdc73c6f89d1edd290d4de34adafdd
Taj Sharma <sharma.tajinder01@gmail.com>
2024-10-23 16:03:15 -0700

    add logic to handle submit

--- insurance-frontend/src/Components/HomeForm.tsx
+++ insurance-frontend/src/Components/HomeForm.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import axios from 'axios';
 
 const HomeForm: React.FC = () =>{
     const [formData, setFormData] = useState({
@@ -16,10 +17,21 @@ const HomeForm: React.FC = () =>{
       setFormData({ ...formData, [name]: value });
     };
   
-    const handleSubmit = (e: React.FormEvent) => {
+    const handleSubmit = async (e: React.FormEvent) => {
       e.preventDefault();
-      console.log(formData);
-      // Add your form submission logic here
+    
+      try {
+        const response = await axios.post('http://localhost:5000/submit-home', formData);
+        
+        if (response.status === 200) {
+          alert('Form submitted successfully!');
+        } else {
+          alert('Failed to submit the form');
+        }
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error submitting the form:', error);
+      }
     };
   
     return (
